e6a1ef2b087f6dee27d2b05f46b7e0fc701de903
moses <moses@DESKTOP-RLH6MN8>
2020-10-07 21:09:33 +0800

    Commit script

---
+++ src/test/java/MakingTheConnection.java
@@ -0,0 +1,36 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class MakingTheConnection {
+
+	public static void main(String[] args) {
+		
+		// Load The Driver
+		try {
+			
+			// Load the driver class
+			Class.forName("org.mariadb.jdbc.Driver");
+			
+			// Defining the datasource for the driver
+			String sourceURL = "jdbc:mariadb://127.0.0.1/nation";
+			
+			// Create a connection through hte DriverManager
+			Connection databaseConnection =
+					DriverManager.getConnection(sourceURL,"root", "root");
+			
+			// We made it!
+			System.out.println("Connection Established Successfully!");
+			
+			// close connection
+			databaseConnection.close();
+		}
+		catch(ClassNotFoundException e) {
+			System.err.println(e);
+		}
+		catch(SQLException e) {
+			System.out.println(e);
+		}
+	}
+
+}
